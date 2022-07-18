bf6b3cbd0fe66b208a3e7a83e7948446b08aa7e6
NicoOre <nicolasorellana619@gmail.com>
2022-07-17 20:17:07 -0500

    Actualizacion

--- scripts/api.js
+++ scripts/api.js
@@ -7,14 +7,14 @@ function comprobar() {
 }
 
 function filtrar() {
-    
+
     var id = document.getElementById('idJuego').value
     var url = 'https://www.freetogame.com/api/game?id=' + id
 
     const realizarPeticion = async () => {
         const res = await fetch(url)
         const data = await res.json()
-        
+
         document.getElementById('imagen').src = data.thumbnail
         document.getElementById('titulo').innerHTML = data.title
         document.getElementById('descripcion').innerHTML = data.description
@@ -33,4 +33,47 @@ function filtrar() {
     }
 
     realizarPeticion()
+}
+
+function nombre() {
+    var nombre = document.getElementById('idJuego1').value
+    for (let i = 1; i < 525; i++) {
+        var url = 'https://www.freetogame.com/api/game?id=' + i
+        const realizarPeticion = async () => {
+            const res = await fetch(url)
+            const data = await res.json()
+            if (data.title == nombre) {
+                document.getElementById('imagen').src = data.thumbnail
+                document.getElementById('titulo').innerHTML = data.title
+                document.getElementById('descripcion').innerHTML = data.description
+                document.getElementById('genero').innerHTML = data.genre
+                document.getElementById('editor').innerHTML = data.publisher
+                document.getElementById('desarrollador').innerHTML = data.developer
+                document.getElementById('fecha').innerHTML = data.release_date
+                document.getElementById('so').innerHTML = data.minimum_system_requirements.os
+                document.getElementById('procesador').innerHTML = data.minimum_system_requirements.processor
+                document.getElementById('ram').innerHTML = data.minimum_system_requirements.memory
+                document.getElementById('graficos').innerHTML = data.minimum_system_requirements.graphics
+                document.getElementById('almacenamiento').innerHTML = data.minimum_system_requirements.storage
+                console.log(data.id)
+            }
+            /*document.getElementById('imagen').src = data.thumbnail
+            document.getElementById('titulo').innerHTML = data.title
+            document.getElementById('descripcion').innerHTML = data.description
+            document.getElementById('genero').innerHTML = data.genre
+            document.getElementById('editor').innerHTML = data.publisher
+            document.getElementById('desarrollador').innerHTML = data.developer
+            document.getElementById('fecha').innerHTML = data.release_date
+
+            document.getElementById('so').innerHTML = data.minimum_system_requirements.os
+            document.getElementById('procesador').innerHTML = data.minimum_system_requirements.processor
+            document.getElementById('ram').innerHTML = data.minimum_system_requirements.memory
+            document.getElementById('graficos').innerHTML = data.minimum_system_requirements.graphics
+            document.getElementById('almacenamiento').innerHTML = data.minimum_system_requirements.storage*/
+
+        }
+
+        realizarPeticion()
+    }
+
 }
